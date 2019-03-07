607ad214f35b10d72ee2ba2322e15277cf63b090
Ranjay Krishna <ranjaykrishna@gmail.com>
2019-03-06 19:52:44 -0800

    adding mlp

---
+++ models/mlp.py
@@ -0,0 +1,56 @@
+"""A simple MLP.
+"""
+
+from collections import OrderedDict
+from torch import nn
+
+import math
+
+
+class MLP(nn.Module):
+    """A simple MLP.
+    """
+
+    def __init__(self, input_size, hidden_size, num_classes,
+                 num_layers=1, dropout_p=0.0):
+        """Constructor for MLP.
+
+        Args:
+            input_size: The number of input dimensions.
+            hidden_size: The number of hidden dimensions for each layer.
+            num_classes: The size of the output.
+            num_layers: The number of hidden layers.
+            dropout_p: Dropout probability.
+        """
+        super(MLP, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_classes = num_classes
+        layers = []
+        for i in range(num_layers):
+            idim = hidden_size
+            odim = hidden_size
+            if i == 0:
+                idim = input_size
+            if i == num_layers-1:
+                odim = num_classes
+            fc = nn.Linear(idim, odim)
+            fc.weight.data.normal_(0.0,  math.sqrt(2. / idim))
+            fc.bias.data.fill_(0)
+            layers.append(('fc'+str(i), fc))
+            if i != num_layers-1:
+                layers.append(('relu'+str(i), nn.ReLU()))
+                layers.append(('dropout'+str(i), nn.Dropout(p=dropout_p)))
+        self.layers = nn.Sequential(OrderedDict(layers))
+
+    def params_to_train(self):
+        return self.layers.parameters()
+
+    def forward(self, x):
+        """Propagate through all the hidden layers.
+
+        Args:
+            x: Input of self.input_size dimensions.
+        """
+        out = self.layers(x)
+        return out
